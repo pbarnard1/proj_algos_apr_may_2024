a7b82333591032f19fa010832bad82040dc11973
unknown <pabarnard@outlook.com>
2024-04-24 18:17:49 -0700

    Added Week 2 Wednesday office hour code - including fixes to algo challenge - and lecture outline

--- office_hours/two_sum.js
+++ office_hours/two_sum.js
@@ -4,29 +4,37 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    // THIS IS FASTER - BUT IS INCORRECT DUE TO A BUG - we'll fix this in Wednesday's office hour and add visuals!
-
+    /* FASTER VERSION with a hash table (object) */
     // Hash table
     let allFoundValues = {};
     for (let k = 0; k < nums.length; k++) {
         let currentValue = nums[k];
-        // Check to see if the value is NOT in the hash table
-        if (!(currentValue in allFoundValues)) {
+        // Check to see if the value is NOT in the hash table - didn't need if statement;
+        // we will save the LAST instance of the value in terms of index
+        // if (!(currentValue in allFoundValues)) {
             allFoundValues[currentValue] = k;
-        }
+        // }
     }
     // console.log(allFoundValues);
     // Loop again through the array to find the indexes where the values add up to the target
     for (let k = 0; k < nums.length; k++) {
         let currentVal = nums[k];
         let otherValue = target - nums[k];
+        // console.log(`The current value at index ${k} is ${currentVal}`);
+        // console.log(`Search for the value ${otherValue}`);
         // Check to see whether the other value exists in the hash map
+        // console.log(otherValue in allFoundValues);
+        // console.log(`Checking to see if ${otherValue} is NOT at index ${k}`)
+        // console.log(allFoundValues[otherValue] != k);
+        // If we find the corresponding value in the hash map AND
+        // this value is at an index OTHER than the current location (index k)
         if (otherValue in allFoundValues && allFoundValues[otherValue] != k) {
             return [k, allFoundValues[otherValue]];
         }
+        // return [-1, -1]; // Arbitrary answer saying that there is no solution
     }
 
-    // SLOWER VERSION - it DOES work!
+    /* SLOWER VERSION that uses a nested for loop */
     // /* Loop through the values in the array */
     // // Left pointer: starting at index 0 and ending at the SECOND to last value (hence the -1)
     // for (let i = 0; i < nums.length - 1; i++) {
